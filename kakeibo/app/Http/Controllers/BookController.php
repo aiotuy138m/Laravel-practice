b38fa2d33865ad31741b066d44f3e966d932b94b
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-23 14:19:00 +0000

    [add]コントローラーのstoreと[fix]createのview

--- kakeibo/app/Http/Controllers/BookController.php
+++ kakeibo/app/Http/Controllers/BookController.php
@@ -24,4 +24,16 @@ class BookController extends Controller
     public function create() {
         return view("books.create");
     }
+    
+    public function store(Request $request){
+        $book = new Book();
+        $book->inout = $request->input("inout");
+        $book->year = $request->input("year");
+        $book->month = $request->input("month");
+        $book->category = $request->input("category");
+        $book->amount = $request->input("amount");
+        $book->save();
+        
+        return redirect()->route('books.show', $book);
+    }
 }
