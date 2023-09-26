647724e720b4cc54ace9dea0e1b278f394e8d6fb
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-26 04:10:31 +0000

    [add]ユーザー以外は閲覧制限

--- kakeibo/app/Http/Controllers/BookController.php
+++ kakeibo/app/Http/Controllers/BookController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Book; //追記
+use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
+    //ログインしていない状態ではログイン画面へリダイレクト
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     function index() {
         //bookテーブルに入っているデータを全てとってくる
-        $books = Book::all();
+        // $books = Book::all();
+        //ログイン機能追加により、全てではなく自分のデータのみ取得するように変更
+        $books = Auth::user()->books;
         //使うビューファイルを指定
         //compactにはビューファイルに送るデータを選択
         return view("books.index", compact("books"));
@@ -18,6 +26,7 @@ class BookController extends Controller
     }
 
     function show(Book $book) {
+        $this->checkMyData($book);
         return view("books.show", compact("book"));
     }
 
@@ -28,16 +37,19 @@ class BookController extends Controller
     public function store(Request $request){
         $book = new Book();
         $book->fill($request->all());
+        $book->user_id = Auth::user()->id;
         $book->save();
 
         return redirect()->route('books.show', $book);
     }
 
     public function edit(Book $book) {
+        $this->checkMyData($book);
         return view("books.edit", compact("book"));
     }
 
     public function update(Request $request, Book $book) {
+        $this->checkMyData($book);
         $book->fill($request->all());
         $book->save();
 
@@ -45,7 +57,14 @@ class BookController extends Controller
     }
 
     public function destroy(Book $book) {
+        $this->checkMyData($book);
         $book->delete();
         return redirect()->route("books.index");
+    }
+    
+    private function checkMyData(Book $book) {
+        if($book->user_id != Auth::user()->id) {
+            return redirect()->route('books.index');
+        }
     }
 }
