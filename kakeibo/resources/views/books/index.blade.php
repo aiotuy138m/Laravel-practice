266842b98825b0912e6d219c79efc1da9c504754
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-09-29 04:57:33 +0000

    [三項演算子に変更]indexの区分のところの表示「1or2」→「収入or支出」

--- kakeibo/resources/views/books/index.blade.php
+++ kakeibo/resources/views/books/index.blade.php
@@ -15,15 +15,7 @@
     @foreach($books as $book)
     <tr>
         <td>{{ $book->year }}年{{ $book->month }}月</td>
-        <td>
-            <?php
-              if ($book->inout == 1):
-                print "収入";
-              else:
-                print "支出";
-              endif;
-            ?>
-        </td>
+        <td>{{ ($book->inout == 1) ? "収入" : "支出" }}</td>
         <td>{{ $book->category }}</td>
         <td>{{ $book->amount }}万円</td>
         <td>
